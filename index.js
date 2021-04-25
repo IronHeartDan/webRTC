3f30a00434d9cb5542f67ca975f3b12598ae2f11
IronHeartDan <danishkhan.dk2.dk16@gmail.com>
2021-04-25 15:40:43 +0530

    Peer To WebRTC

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.port || 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile("public/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id + "Connected...");
+
+  socket.on("check", (room) => {
+    let exists = io.sockets.adapter.rooms.get(room);
+    if (exists === undefined) {
+      console.log("Joining...");
+      socket.join(room);
+      socket.emit("waiting");
+    } else {
+      console.log("Found......");
+      if (exists.size < 2) {
+        console.log("Joining...");
+        socket.join(room);
+        socket.broadcast.to(room).emit("joined");
+      } else {
+        socket.emit("full");
+      }
+    }
+  });
+
+  socket.on("call", (data) => {
+    let room = data.room;
+    socket.broadcast.to(room).emit("call", data);
+  });
+
+  socket.on("answer", (data) => {
+    let room = data.room;
+    socket.broadcast.to(room).emit("answer", data);
+  });
+
+  socket.on("disconnecting", () => {
+    let rooms = Array.from(socket.rooms);
+    if (rooms.length > 1) {
+      rooms.forEach((room) => {
+        socket.broadcast.to(room).emit("closed");
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log(socket.id + "Disconnected...");
+  });
+});
+
+server.listen(port, () => {
+  console.log("Listening At ::" + port);
+});
